7cf3909141c613c703e2fab0d038026fa941cb68
Franco Albornoz <franco.albornoz@despegar.com>
2019-05-02 16:08:01 -0300

    Build correct SpanObject

--- build.sbt
+++ build.sbt
@@ -4,7 +4,7 @@ version := "0.1"
 
 scalaVersion := "2.12.8"
 
-libraryDependencies ++= Seq(
+libraryDependencies ++= Seq("org.scalatest" %% "scalatest" % "3.0.5",
   "org.apache.kafka" % "kafka-clients" % "2.1.1",
   "org.apache.kafka" %% "kafka-streams-scala" % "2.1.1",
   "org.json4s" %% "json4s-ext" % "3.6.5",
--- src/main/scala/com/despegar/www/haystack/jaeger_collector/App.scala
+++ src/main/scala/com/despegar/www/haystack/jaeger_collector/App.scala
@@ -5,7 +5,10 @@ import java.time.format.DateTimeFormatter
 import java.util.Properties
 import java.util.concurrent.TimeUnit
 
-import com.expedia.open.tracing.span.Span
+import org.json4s.CustomSerializer
+import org.json4s.JsonAST._
+import org.json4s.JsonDSL._
+import com.expedia.open.tracing.span.{Log, Span, Tag}
 import org.apache.kafka.streams.scala.ImplicitConversions._
 import org.apache.kafka.streams.scala._
 import org.apache.kafka.streams.scala.kstream._
@@ -32,25 +35,13 @@ object App {
       p
     }
 
-    val datePattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
-    val durationPattern = "s.SSS's'"
-
-    object StringToLong extends CustomSerializer[Long](format => (
-      { case JString(x) =>  try {
-        DateTime.parse(x, DateTimeFormat.forPattern(datePattern)).getMillis
-      } catch {
-        case _: Exception => DateTime.parse(x, DateTimeFormat.forPattern(durationPattern)).getMillis * 1000
-      }},
-      { case x: Long => JInt(x) }))
-
-
-    implicit val formats: Formats = DefaultFormats + StringToLong
+    implicit val formats: Formats = DefaultFormats + SpanCustomDeserializer
 
     val builder = new StreamsBuilder
     val jaegerTraces: KStream[String,String] = builder.stream[String, String]("jaeger-spans")
     val haystackTraces: KStream[String, Span] = jaegerTraces
       .flatMapValues( (_, b) => List(parse(b).extract[Span]) )
-    haystackTraces.to("test-topic")
+    haystackTraces.to("proto-spans")
 
     val streams: KafkaStreams = new KafkaStreams(builder.build(), props)
     streams.start()
@@ -60,3 +51,41 @@ object App {
     }
   }
 }
+
+object SpanCustomDeserializer extends CustomSerializer[Span](format => ( {
+
+  case jsonObj: JObject =>
+    implicit val formats: Formats = DefaultFormats + StringToLong
+
+    val traceId = (jsonObj \ "traceId").extract[String]
+    val spanId = (jsonObj \ "spanId").extract[String]
+    val parentSpanId = ""
+    println(s"JObject: $jsonObj")
+    val serviceName = (jsonObj \ "process" \ "serviceName").extract[String]
+    val operationName = (jsonObj \ "operationName").extract[Option[String]]
+    val startTime = (jsonObj \ "startTime").extract[Long]
+    val duration = (jsonObj \ "duration").extract[Option[Long]]
+    val logs = (jsonObj \ "logs").extract[Seq[Log]]
+    val tags = (jsonObj \ "tags").extract[Seq[Tag]]
+
+    val span = Span(traceId, spanId, parentSpanId, serviceName, operationName.getOrElse("operation-not-found"), startTime, duration.getOrElse(0L), logs, tags)
+    println(span)
+    span
+}, {
+  case span: Span =>
+      ("traceId" -> span.traceId) ~
+      ("spanId" -> span.spanId) ~
+      ("parentSpanId" -> span.parentSpanId)
+
+}))
+
+object StringToLong extends CustomSerializer[Long](format => (
+  { case JString(x) =>  try {
+    val datePattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
+    DateTime.parse(x, DateTimeFormat.forPattern(datePattern)).getMillis * 1000
+  } catch {
+    case _: Exception =>
+      val durationPattern = "s.SSS's'"
+      DateTime.parse(x, DateTimeFormat.forPattern(durationPattern)).getMillis * 1000
+  }},
+  { case x: Long => JInt(x) }))
